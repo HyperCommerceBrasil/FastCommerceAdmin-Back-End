fdd93b6316edddfbb65b228416324ea3994b309f
Thales Morais <thalesmorais@pop-os.localdomain>
2021-03-14 12:04:53 -0300

    Fixed paste dist

--- dist/shared/infra/http/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_app.default.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
-});
