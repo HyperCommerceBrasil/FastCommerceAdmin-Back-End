6d0d44b9afb52a2f119e4d502ad44a3e37660368
Thales Morais <thales.morais21@gmail.com>
2021-07-25 18:20:52 -0300

    added code in messagem

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -29,6 +29,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
+      code: err.code,
     });
   }
 
