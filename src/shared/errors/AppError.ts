1923beff812d6936800ee4966be83bcbac035053
Thales Morais <thales.morais21@gmail.com>
2021-07-25 18:17:17 -0300

    Added code

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -2,10 +2,12 @@ class AppError {
   public readonly message: string;
 
   public readonly statusCode: number;
+  public readonly code: number;
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, statusCode = 400, code = 0) {
     this.message = message;
     this.statusCode = statusCode;
+    this.code = code;
   }
 }
 
